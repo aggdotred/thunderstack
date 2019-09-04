6039fb7f2a979283b30551c8c6740a1b91ba2b4c
Adan Gutierrez <hevyw8@gmail.com>
2019-09-03 19:08:31 -0500

    added day functionality to index

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, g, request
+from datetime import datetime
 import sqlite3
 
 app = Flask(__name__)
@@ -22,9 +23,33 @@ def close_db(error):
         g.sqlite_db.close()
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("home.html")
+    db = get_db()
+
+    if request.method == "POST":
+        date = request.form["date"]
+
+        dt = datetime.strptime(date, "%Y-%m-%d")
+        database_date = datetime.strftime(dt, "%Y%m%d")
+
+        db.execute("insert into log_date (entry_date) values (?)", [database_date])
+        db.commit()
+
+    cur = db.execute("select entry_date from log_date order by entry_date desc")
+    results = cur.fetchall()
+
+    pretty_results = []
+
+    for result in results:
+        single_date = {}
+
+        d = datetime.strptime(str(result["entry_date"]), "%Y%m%d")
+        single_date["entry_date"] = datetime.strftime(d, "%B %d %Y")
+
+        pretty_results.append(single_date)
+
+    return render_template("home.html", results=pretty_results)
 
 
 @app.route("/view")
